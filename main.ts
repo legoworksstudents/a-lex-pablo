80806215404bac4094eaa366c3bedf13e1d1bb4f
LegoWorksStudent <122985953+legoworksstudents@users.noreply.github.com>
2025-03-26 18:14:51 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,17 @@
 basic.forever(function () {
-    if (30 < PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm)) {
-        neZha.setServoSpeed(neZha.ServoList.S1, 30)
-        neZha.setServoSpeed(neZha.ServoList.S2, -30)
-    } else if (30 < PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J2, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm)) {
+    if (40 < PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm)) {
         neZha.setServoSpeed(neZha.ServoList.S1, -30)
+        neZha.setServoSpeed(neZha.ServoList.S2, 0)
+        basic.pause(1000)
+    } else if (40 < PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J2, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm)) {
+        neZha.setServoSpeed(neZha.ServoList.S1, 0)
         neZha.setServoSpeed(neZha.ServoList.S2, 30)
+        basic.pause(1000)
+        neZha.setServoSpeed(neZha.ServoList.S1, -30)
+        neZha.setServoSpeed(neZha.ServoList.S1, -30)
+        basic.pause(1000)
     } else {
-        neZha.setServoSpeed(neZha.ServoList.S1, 30)
+        neZha.setServoSpeed(neZha.ServoList.S1, -30)
         neZha.setServoSpeed(neZha.ServoList.S2, 30)
     }
 })
